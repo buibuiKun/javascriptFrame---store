454bc5756811664378f1d50ef6e4468ca43f6ac1
lz123456 <18818408064@163.com>
2019-07-12 18:02:41 +0800

    vue 框架项目模板初始化

---
+++ vue/2.x-template/app-cli/src/components/navigatior/onNavigationBarOnTop.vue
@@ -0,0 +1,21 @@
+<template>
+	<div onNavigationBarOnTop>
+		<span></span>
+	</div>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+
+			}
+		}
+	}
+</script>
+
+<style lang='scss'>
+	[onNavigationBarOnTop] {
+
+	}
+</style>
---
+++ vue/2.x-template/app-cli/src/page/index/index.vue
@@ -0,0 +1,21 @@
+<template>
+	<div indexForApp>
+		<p>主页</p>
+	</div>
+</template>
+
+<script>
+
+	export default {
+		data() {
+			return {
+
+			}
+		}
+	}
+
+</script>
+
+<style lang='scss'>
+
+</style>
